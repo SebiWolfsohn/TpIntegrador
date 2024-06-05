ed0139f6e81ba9f7ab2ca281f3aedc0fee7da16b
Manu Weinstein <manuweinstein@MacBook-Pro-de-Manu.local>
2024-06-05 12:10:18 -0300

    agrego

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let queryString = location.search
+let queryStringToObject = new URLSearchParams(queryString);
+let id = queryStringToObject.get('id');
+console.log("id personaje", id);
+let url = `https://fakestoreapi.com/docs${id}`;
+
+fetch(url)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+    let characterList = document.querySelector ('.characterList');
+    characterList.innerHTML += data.results [0].characterList.personajes;
+  console.log(data);
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
+
+
