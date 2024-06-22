3d032359bdc145435c3061dedd56dd9c7f2894bc
Manu Weinstein <manuweinstein@MacBook-Pro-de-Manu.local>
2024-06-21 22:58:23 -0300

    agrego + carrito FALTA ALGO

--- js/cart.js
+++ js/cart.js
@@ -1,32 +1,45 @@
+let queryString = location.search
+let queryStringToObject = new URLSearchParams(queryString);
+let id = queryStringToObject.get('id');
+let url = `https://fakestoreapi.com/products/${id}`;
 
 let carrito = () => {
-    let inCarrito = document.getElementById('containerCart');
     let productosCarrito = JSON.parse(localStorage.getItem('cart'))||[];
-    containerCart.innerHTML = '';
+    containerItems.innerHTML = '';
     if (productosCarrito.length == 0) {
         productosCarrito.innerText = 'Su carrito esta vacio'
         erroremail.style.display = 'block';
     } else {
         for (let i = 0; i < productosCarrito.length; i++) {
-            let producto = productosCarrito[i];
-            
+            let productos = productosCarrito[i];
+            datosproducto(productos)
+            fetch(url)
+             .then (function (response) {
+                return response.json()
+            })
+             .then (function (data) {
+                let producto = document.querySelector('.containeritems');
+                let element = data;
+                producto.innerHTML = ` <div class='producto'>
+                <h2>${element.title}</h2>
+                <p>Precio: $${element.price}</p>
+                <img src='${element.image}' alt='${element.title}'
+                </div>`;
+                containerItems.innerHTML = data;
+            })
+             .catch (function (error) {
+                console.log("Error al obtener el producto:" + error);
+            });
         }
+        data += `
+        <button class='finalizarCompra'>Finalizar compra</button>`;
+        containerItems.innerHTML = data;
+        document.querySelector('.finalizarCompra').addEventListener('click', compraFinalizada,)
     }
 }
 
-
-let agregar = [carrito];
-let agregarJson=JSON.stringify(agregar);
-console.log(agregar);
-console.log(agregarJson);
-
-localStorage.setItem('agregar', JSON.stringify(agregarJson))
-
-let Cart = localStorage.getItem('carrito');
-agregar.push(carrito);
-console.log(carrito);
-
-let carritoParseado = JSON.parse(carrito);
-for (let i = 0; i < carritoParseado.length; i++) {
-  console.log = (carritoParseado[i]);
+let compraFinalizada = () => {
+    localStorage.removeItem('cart');
+    alert('Gracias por su compra!')
 }
+
