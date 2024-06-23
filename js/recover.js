84af74ebb466f539373041c7eacefd470c821609
SebiWolfsohn <swolfsohn@udesa.edu.ar>
2024-06-23 15:27:34 -0300

    mini error corrijo

--- js/recover.js
+++ js/recover.js
@@ -2,22 +2,20 @@ let erroremail = document.querySelector('.mensajeEmail');
 let errorcheckbox = document.querySelector('.mensajeCheckbox');
 let errorForms = document.querySelector('.errorForms')
 let form = document.querySelector('#forms');
+let email = document.querySelector('.email')
+let checkbox = document.querySelector('.checkbox')
+let forms = document.querySelector('#forms')
 let user = {}
 
-form.addEventListener('submit', function (e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
- 
-    let email = document.querySelector('.email')
-    let checkbox = document.querySelector('.checkbox')
-    let forms = document.querySelector('#forms')
-
-    if (email.value == '' || email.value.length < 7){
+    if (email.value == '' || email.value.length < 7) {
         erroremail.innerText = 'Porfavor escriba su mail'
         erroremail.style.display = 'block';
-    } else if (checkbox.value == '' || checkbox.value){
+    } else if (checkbox.value == '' || checkbox.value) {
         errorcheckbox.innerHTML = 'Por favor acepte el campo Quiero recuperar mi contraseña'
         errorcheckbox.style.display = 'block';
-    } else if (!forms.value){
+    } else if (!forms.value) {
         forms.innerHTML = 'Recibirás un email con las instrucciones para recuperar tu contraseña'
         errorForms.style.display = 'block';
     } else {
