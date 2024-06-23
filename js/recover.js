7053ddfa9cd7a888eb8427ff83fc83728c381014
Hannahalfie6 <halfie@udesa.edu.ar>
2024-06-23 18:18:21 -0300

    recover

--- js/recover.js
+++ js/recover.js
@@ -1,30 +1,43 @@
-let erroremail = document.querySelector('.mensajeEmail');
-let errorcheckbox = document.querySelector('.mensajeCheckbox');
-let errorForms = document.querySelector('.errorForms')
 let form = document.querySelector('#forms');
+let email = document.querySelector('.email');
+let erroremail = document.querySelector('.mensajeEmail');
+let checkbox = document.querySelector('#checkbox'); 
+let errorcheckbox = document.querySelector('.mensajeCheckbox');
+let respuestaFinal = document.querySelector ('.respuestaFinal');
+let linkLogin = document.querySelector('.linkLogin');
 let user = {}
+
+linkLogin.style.display = 'none';
 
 form.addEventListener('submit', function (e){
     e.preventDefault();
- 
-    let email = document.querySelector('.email')
-    let checkbox = document.querySelector('.checkbox')
-    let forms = document.querySelector('#forms')
+    let isValid = true;
 
-    if (email.value == '' || email.value.length < 7){
-        erroremail.innerText = 'Porfavor escriba su mail'
+   if (email.value === '' || email.value.length < 7) {
+        erroremail.innerText = 'Por favor escriba su email.';
         erroremail.style.display = 'block';
-    } else if (checkbox.value == '' || checkbox.value){
-        errorcheckbox.innerHTML = 'Por favor acepte el campo Quiero recuperar mi contraseña'
-        errorcheckbox.style.display = 'block';
-    } else if (!forms.value){
-        forms.innerHTML = 'Recibirás un email con las instrucciones para recuperar tu contraseña'
-        errorForms.style.display = 'block';
+        isValid = false;
     } else {
-        user.email = email.value;
-        user.checkbox = checkbox.value;
-        localStorage.setItem('user', JSON.stringify(user))
-        this.submit();
+        erroremail.style.display = 'none';
     }
-}
-)
+
+  if (!checkbox.checked) {
+        errorcheckbox.innerHTML = 'Por favor acepte el campo';
+        errorcheckbox.style.display = 'block';
+        isValid = false;
+    } else {
+        errorcheckbox.style.display = 'none';
+    }
+
+    if (isValid) {
+        form.style.display = 'none';
+        user.email = email.value;
+        user.checkbox = checkbox.checked;
+        localStorage.setItem('user', JSON.stringify(user));
+        respuestaFinal.innerText = 'Recibirás un email con las instrucciones para recuperar tu contraseña';
+        respuestaFinal.style.display = 'block';
+        linkLogin.style.display = 'block';
+        form.reset();
+    }
+    
+});
