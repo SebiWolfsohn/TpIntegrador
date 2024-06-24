97c301b88fd16e98ba1c139cee3dbaa0a115d591
Manu Weinstein <manuweinstein@MacBook-Pro-de-Manu.local>
2024-06-24 00:15:15 -0300

    recover terminado

--- js/recover.js
+++ js/recover.js
@@ -2,34 +2,34 @@ document.addEventListener('DOMContentLoaded', function() {
     let form = document.querySelector('#form');
     let email = document.querySelector('#email');
     let erroremail = document.querySelector('.invalid-feedback.email');
-    let checkbox = document.querySelector('#checkbox'); 
+    let checkbox = document.querySelector('#checkbox');
     let errorcheckbox = document.querySelector('.invalid-feedback.checkbox');
-    let respuestaFinal = document.querySelector ('#respuestaFinal');
+    let respuestaFinal = document.querySelector('#respuestaFinal');
     let linkLogin = document.querySelector('#linkLogin');
 
- form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    erroremail.style.display = 'none';
-    errorcheckbox.style.display = 'none';
-    respuestaFinal.textContent = '';
-    linkLogin.style.display = 'none';
-    let respuesta = true;
-    if (email.value == '' || email.value.length < 7){
-        erroremail.innerText = 'Por favor escriba su mail'
-        erroremail.style.display = 'block';
-        respuesta = false;
-    } 
-    else if (!checkbox.checked){
-        errorcheckbox.innerText = 'Por favor acepte el campo Quiero recuperar mi contraseña'
-        errorcheckbox.style.display = 'block';
-        respuesta = false;
-    } 
-    if (respuesta) {
-        respuestaFinal.innerText = 'Recibirás un email con las instrucciones para recuperar tu contraseña';
-        linkLogin.style.display = 'block';
-        let user = {};
-         user.email = email.value;
-         user.checkbox = checkbox.value;
-    } localStorage.setItem('user', JSON.stringify(user))
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        erroremail.style.display = 'none';
+        errorcheckbox.style.display = 'none';
+        respuestaFinal.textContent = '';
+        linkLogin.style.display = 'none';
+        let valid = true;
+
+        if (email.value === '' || email.value.length < 7) {
+            erroremail.innerText = 'Por favor escriba su email';
+            erroremail.style.display = 'block';
+            valid = false;
+        }
+
+        if (!checkbox.checked) {
+            errorcheckbox.innerText = 'Por favor acepte el campo Quiero recuperar mi contraseña';
+            errorcheckbox.style.display = 'block';
+            valid = false;
+        }
+
+        if (valid) {
+            respuestaFinal.innerText = 'Recibirás un email con las instrucciones para recuperar tu contraseña';
+            linkLogin.style.display = 'block';
+        }
     });
 });
